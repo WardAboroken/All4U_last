50b95cbe013b92c1eda73df763c79550a91c50d2
WardAboroken <wardaboroken@gmail.com>
2024-09-20 12:55:24 +0300

    Add files via upload

--- fe_copy/src/App.js
+++ fe_copy/src/App.js
@@ -7,10 +7,16 @@ import UserTypeSelection from "./pages/UserTypeSelection";
 import ShopOwnerSignUp from "./pages/ShopOwnerSignUp";
 import ShopMainPage from "./pages/ShopMainPage";
 import CustomerOrdersHistory from "./pages/CustomerOrdersHistory";
-import EditProfile from "./pages/EditProfile";
+import EditCustomerProfile from "./pages/EditCustomerProfile";
 import ShopOwnerMainPage from "./pages/ShopOwnerMainPage";
 import ResetPass from "./pages/ResetPass";
-
+import CategoryPage from "./pages/CategoryPage";
+import ProductPage from "./pages/ProductPage";
+import BasketCart from "./pages/BasketCart";
+import AdminMainPage from "./pages/AdminMainPage";
+import ShopOwnerOrdersPage from "./pages/ShopOwnerOrdersPage.jsx"
+import ShopOwnerProductsPage from "./pages/ShopOwnerProductsPage.jsx";
+import EditShopOwnerProfile from "./pages/EditShopOwnerProfile";
 
 function App() {
   const projectName = "Test Project";
@@ -26,12 +32,30 @@ function App() {
         <Route path="/ShopMainPage" element={<ShopMainPage />} />
         <Route path="/ResetPass" element={<ResetPass />} />
         <Route path="/ShopOwnerMainPage" element={<ShopOwnerMainPage />} />
+        <Route path="/ShopOwnerOrdersPage" element={<ShopOwnerOrdersPage />} />
+        <Route
+          path="/ShopOwnerProductsPage"
+          element={<ShopOwnerProductsPage />}
+        />
+        <Route path="/ShopMainPage/:categoryName" element={<CategoryPage />} />
+        <Route path="/AdminMainPage" element={<AdminMainPage />} />
         <Route
           path="/CustomerOrdersHistory"
           element={<CustomerOrdersHistory />}
         />
-        <Route path="/EditProfile" element={<EditProfile />} />
+        <Route path="/EditCustomerProfile" element={<EditCustomerProfile />} />
+        <Route path="/Product/:catalogNumber" element={<ProductPage />} />
+        <Route path="/BasketCart" element={<BasketCart />} />
+        <Route
+          path="/EditShopOwnerProfile"
+          element={<EditShopOwnerProfile />}
+        />
       </Routes>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      />
+      ;
     </div>
   );
 }
