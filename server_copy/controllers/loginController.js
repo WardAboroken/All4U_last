af70eb99ef4684fc6cbfd86442b534dbc23c066e
WardAboroken <wardaboroken@gmail.com>
2024-09-20 12:59:21 +0300

    Add files via upload

--- server_copy/controllers/loginController.js
+++ server_copy/controllers/loginController.js
@@ -3,19 +3,40 @@
 // Import necessary modules and functions
 const checkUserType = require("../database/queries/login");
 const findUser = require("../database/queries/findUser");
+const getWorkerInfo = require("../database/queries/getWorkerInfo");
+const getCustomerInfo = require("../database/queries/getCustomerInfo");
 
 // Handle user login request
 const handleLogin = async (req, res) => {
   try {
     const user = req.body;
-    console.log("Request body:", req.body); // Log entire req.body to inspect structure
+    // console.log("Request body:", req.body); // Log entire req.body to inspect structure
 
     // Check if user exists in the database
     const findResult = await findUser(user);
 
     if (findResult.success) {
-      // Determine user type
       const userTypeResult = await checkUserType(user);
+      let userInfo = {};
+
+      //newwwwwwwwwww
+      if (userTypeResult === "normal") {
+        const customerInfo = await getCustomerInfo(user);
+        if (customerInfo) {
+          userInfo = { ...user, ...customerInfo };
+        }
+      } else if (
+        userTypeResult === "admin" ||
+        userTypeResult === "businessowner"
+      ) {
+        const workerInfo = await getWorkerInfo(user);
+        if (workerInfo) {
+          userInfo = { ...user, ...workerInfo };
+        }
+      }
+
+      // Determine user type
+      // const userTypeResult = await checkUserType(user);
       // req.session.userName = req[1];
 
       // Store user type in session based on userTypeResult
@@ -26,15 +47,22 @@ const handleLogin = async (req, res) => {
           message: "User authenticated successfully",
           redirectUrl: "/ShopMainPage", // Redirect URL for normal users
         });
-      } else if (userTypeResult === "worker") {
-         req.session.userName = user.userName;
+      } else if (userTypeResult === "businessowner" && userInfo.status === 1) {
+        req.session.userName = user.userName;
         req.session.userType = "worker";
         return res.status(200).json({
           message: "User authenticated successfully",
           redirectUrl: "/ShopOwnerMainPage", // Redirect URL for workers
         });
+      } else if (userTypeResult === "admin" && userInfo.status === 1) {
+        req.session.userType = "admin";
+        req.session.userName = user.userName;
+        return res.status(200).json({
+          message: "User authenticated successfully",
+          redirectUrl: "/AdminMainPage", // Redirect URL for normal users
+        });
       } else {
-        return res.status(400).json({ message: "Invalid user type" });
+        return res.status(400).json({ message: "you dont have permission" });
       }
     } else {
       // Send error response if user not found
